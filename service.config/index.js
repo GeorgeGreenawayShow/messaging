0bc41c0fa90e0ac0b66f619fce1a78b229b17a37
Cameron Fleming <cameron@nevexo.space>
2020-03-28 14:32:59 +0000

    Add config service

---
+++ service.config/index.js
@@ -0,0 +1,28 @@
+// GGRM Configuration Service
+// MIT License 
+
+const express = require("express");
+const yaml = require("yaml");
+const fs = require("fs");
+
+let config = {}
+const app = new express();
+
+fs.readFile("./config.yaml", (error, data) => {
+    if (error) {
+        console.error(error);
+    }else {
+        config = yaml.parse(data.toString());
+        app.listen(3000, () => {
+            console.log("[configuration] Now listening on port 3000.")
+        });
+    }
+})
+
+app.get("/:service", (req, res) => {
+    const obj = {
+        "global": config['global'] || {},
+        "service": config[req.params.service] || {}
+    }
+    res.json(obj)
+})
