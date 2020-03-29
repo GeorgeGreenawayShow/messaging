c2310cd3b3a433f28e432f7ecad8868ec4fefbe0
Cameron Fleming <cameron@nevexo.space>
2020-03-29 18:26:09 +0100

    Create setup user on mongo init

--- mongo-schemas/auth.js
+++ mongo-schemas/auth.js
@@ -1,3 +1,13 @@
 // Collections used by service.auth
 
 db.createCollection("users")
+
+let user_object = {
+    "username": "setup",
+    "password": "$2b$10$8LVeAjHSrMkXCNKNXNNGO.ju6rcpswz122k6yvctfRo3l/p4mSb6W",
+    "token": null,
+    "token_expires": null,
+    "avatar": ""
+}
+
+db.users.insertOne(user_object)
